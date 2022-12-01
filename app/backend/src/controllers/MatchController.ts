d1fb137d0eaf75efaffcfe790a81c800692a6231
Daniel Saraiva <danieldsvn@gmail.com>
2022-12-01 14:05:39 -0300

    Implementado busca por query no atributo inProgress na rota GET /matches. Tipagem de inProgress precisa alterar

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -5,6 +5,12 @@ import MatchService from '../services/MatchService';
 export default class MatchController {
   static async getAll(req: Request, res: Response) {
     try {
+      const { inProgress } = req.query;
+      if (inProgress) {
+        const response = await MatchService.getByQuery(inProgress as any);
+        const { status, payload } = response;
+        return res.status(status).json(payload);
+      }
       const response = await MatchService.getAll();
 
       const { status, payload } = response;
--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -15,5 +15,20 @@ export default class MatchService {
     return { status: 200, payload: response };
   }
 
-  // static async getById()
+  static async getByQuery(inProgress: string) {
+    let boolean = null;
+    if (inProgress === 'false') boolean = false;
+    if (inProgress === 'true') boolean = true;
+    const response = await MatchModel.findAll({
+      include: [{
+        model: TeamModel, as: 'teamHome', attributes: { exclude: ['id'] },
+      }, {
+        model: TeamModel, as: 'teamAway', attributes: { exclude: ['id'] },
+      },
+      ],
+      where: { inProgress: boolean },
+    });
+
+    return { status: 200, payload: response };
+  }
 }
